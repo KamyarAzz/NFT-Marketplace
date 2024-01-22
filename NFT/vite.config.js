cd8669d6b29226b762687e66cd42de3b2447b57e
KamyarAzz <kamyar2019azizi@gmail.com>
2024-01-22 22:07:45 +0330

    -initial commit

---
+++ NFT/vite.config.js
@@ -0,0 +1,9 @@
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [
+    autoprefixer()
+    // Add other plugins as needed
+  ],
+  // Add other configuration options if necessary
+};
